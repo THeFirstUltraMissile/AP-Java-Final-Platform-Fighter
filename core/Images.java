bf0489699dbb8493c763731124f1cb8ac8277d7a
bpm-002 <bpm10012@gmail.com>
2026-04-19 23:15:21 -0400

    Add tetrisBackground to Images class
    Added a new tetrisBackground image to the Images class.

--- core/Images.java
+++ core/Images.java
@@ -7,6 +7,7 @@ public class Images {
 
     public static Image blankBackground;
     public static Image shrineBackground;
+    public static Image tetrisBackground;
 
     public static Image sukunaIcon;
     public static Image gojoIcon;
@@ -14,6 +15,8 @@ public class Images {
     public static void loadImages() throws SlickException {
         blankBackground = new Image("media/blankBackground.png");
         shrineBackground = new Image("media/sprites/stages/stage_malfunctioningshrine.png");
+        tetrisBackground = new Image("media/sprites/stages/stage_tetris2x.png");
+
         sukunaIcon = new Image("media/sprites/kaisen/ryomensukuna/assets/icon_ryomensukuna512.png");
         gojoIcon = new Image("media/sprites/kaisen/gojosatoru/assets_gojosatoru/icon_gojosatoru512.png");
     }
