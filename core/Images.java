e2924e8dddbd6f2b3b808491c00813edfc959712
big rat <itsthesaltlamp@gmail.com>
2026-05-18 21:13:16 -0400

    Fix images

--- core/Images.java
+++ core/Images.java
@@ -11,9 +11,13 @@ public class Images {
     public static Image shrineBackground;
     public static Image tetrisBackground;
     public static Image terraBackground;
+    public static Image menuBackground;
+    public static Image tutorialBackground;
 
     public static Image sukunaIcon;
     public static Image gojoIcon;
+
+    public static Image menuLogo;
 
     public static SpriteSheet hollowPurpleSpriteSheet;
     public static Animation hollowPurpleAnimation;
@@ -27,11 +31,14 @@ public class Images {
         blankBackground = new Image("media/blankBackground.png");
         shrineBackground = new Image("media/sprites/stages/stage_malfunctioningshrine.png");
         tetrisBackground = new Image("media/sprites/stages/stage_tetris2x.png");
+        menuBackground = new Image("media/sprites/menus/missingHD.png");
         terraBackground = new Image("media/sprites/stages/terrariaStage.png");
+        tutorialBackground = new Image("media/sprites/menus/TutorialScreen.png");
 
         sukunaIcon = new Image("media/sprites/kaisen/ryomensukuna/assets/icon_ryomensukuna512.png");
         gojoIcon = new Image("media/sprites/kaisen/gojosatoru/assets_gojosatoru/icon_gojosatoru512.png");
-
+        
+        menuLogo = new Image("media/sprites/menus/missingLogo.png");
         SpriteSheet walking = new SpriteSheet("media/sprites/effects/effects_sprinting1.png", 128, 128);
 
         hollowPurpleSpriteSheet = new SpriteSheet("media/sprites/kaisen/gojosatoru/limitless/purple/gojo_unlimitedpurple.png",512,512);
